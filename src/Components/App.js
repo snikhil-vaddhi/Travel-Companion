8f588b0d6b218659861ca3f5fe32fc02f2150e19
nightblood18 <114045738+nightblood18@users.noreply.github.com>
2024-04-25 19:03:10 -0500

    initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import { PackagingList } from "./PackagingList";
+import { Stats } from "./Stats";
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: true },
+//   { id: 3, description: "Charger", quantity: 1, packed: false },
+// ];
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleItems(item) {
+    setItems((items) => [...items, item]);
+  }
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+  function handleResetDefaults() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all the items?"
+    );
+
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onHandleItems={handleItems} />
+      <PackagingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItems={handleToggleItem}
+        onResetDefaults={handleResetDefaults}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
---
+++ src/Components/Form.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export default function Form({ onHandleItems }) {
+  const [description, setDescription] = useState("");
+  const [select, setSelect] = useState("1");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, select, packed: false, id: Date.now() };
+    onHandleItems(newItem);
+    setDescription("");
+    setSelect(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select value={select} onChange={(e) => setSelect(e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
