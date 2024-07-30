884d0de051214be1d4715d3c53301b93e19ec99b
nightblood18 <114045738+nightblood18@users.noreply.github.com>
2024-07-30 14:00:00 +0100

    handled an extra edge case

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,6 +3,7 @@ import Logo from "./Logo";
 import Form from "./Form";
 import { PackagingList } from "./PackagingList";
 import { Stats } from "./Stats";
+// test data stored in the code itself
 // const initialItems = [
 //   { id: 1, description: "Passports", quantity: 2, packed: false },
 //   { id: 2, description: "Socks", quantity: 12, packed: true },
