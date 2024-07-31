c0fd1cd01bca2226080834b338c5fc6c8282ee50
nightblood18 <114045738+nightblood18@users.noreply.github.com>
2024-07-31 14:00:00 +0100

    handled an edge case

--- src/Components/App.js
+++ src/Components/App.js
@@ -13,6 +13,7 @@ export default function App() {
   const [items, setItems] = useState([]);
 
   function handleItems(item) {
+    // destrcuturing
     setItems((items) => [...items, item]);
   }
   function handleDeleteItem(id) {
